1c9c9520e462c52e4a6547ed49931f58aa6af3f6
thomaslam <tuananhlam95@gmail.com>
2015-01-03 15:14:28 -0500

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,12 +1,7 @@
 class VisitorsController < ApplicationController
 
 	def new
-		Rails.logger.debug 'DEBUG: entering new method'
 		@owner = Owner.new
-		Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
-		# render 'visitors/new', :layout => false
-		flash.now[:notice] = "Welcome!"
-		flash.now[:alert] = "My birthday is soon."
 	end
 	
 end
